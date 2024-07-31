055f4cead5f73e7e42137906742f40e36a0298d0
Alutta <Aluttamatambo5@gmail.com>
2024-07-31 19:15:35 +0200

    added functions

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ let messageEl = document.getElementById(message-el)
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl =documetns.getElementById("player-el")
+
+playerEl.textContext = player.name = ": $" +player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor ( Math.random()*13) + 1
+    if (randomNumber> 10) {
+        return 10
+    } else if(randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = 
+}
